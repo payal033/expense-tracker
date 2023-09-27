2dcd5be80124172bfb72c996b377b638b9053114
Payal <payalkarwa99@gmail.com>
2023-09-28 01:09:09 +0530

    conditional rendering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,20 +16,48 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  // conditional rendering option 3
+  let expenseContent = <p>No expenses found!</p>;
+  if (filteredExpenses.length > 0) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onFilterSelect={showFiteredData}
       />
-      {filteredExpenses.map((expense) => (
+      {/* conditional rendering option 1 */}
+      {/* {filteredExpenses.length === 0 ? <p>No expenses found</p> : filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
           amount={expense.amount}
           date={expense.date}
         />
-      ))}
+      ))} */}
+
+      {/* conditional rendering option 2 */}
+      {/* {filteredExpenses.length === 0 && <p>No expenses found!</p>}
+      {filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      )) } */}
+
+      {/* conditional rendering option 3 */}
+      {expenseContent}
     </Card>
   );
 };
