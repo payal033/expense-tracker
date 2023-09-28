b116838884a2434ec046599000bd886ea420add0
Payal <payalkarwa99@gmail.com>
2023-09-28 12:12:52 +0530

    conditional return statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilterYear] = useState("2021");
@@ -16,48 +16,13 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  // conditional rendering option 3
-  let expenseContent = <p>No expenses found!</p>;
-  if (filteredExpenses.length > 0) {
-    expenseContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onFilterSelect={showFiteredData}
       />
-      {/* conditional rendering option 1 */}
-      {/* {filteredExpenses.length === 0 ? <p>No expenses found</p> : filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))} */}
-
-      {/* conditional rendering option 2 */}
-      {/* {filteredExpenses.length === 0 && <p>No expenses found!</p>}
-      {filteredExpenses.length > 0 && filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      )) } */}
-
-      {/* conditional rendering option 3 */}
-      {expenseContent}
+      <ExpensesList filteredExpenses={filteredExpenses} />
     </Card>
   );
 };
